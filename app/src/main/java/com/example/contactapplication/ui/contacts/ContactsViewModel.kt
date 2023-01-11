cd07491abd4782001025d1c918e16faa169c05eb
phanminhphu159 <phanminhphu159@gmail.com>
2023-01-11 10:15:10 +0700

    first update

--- app/src/main/java/com/example/contactapplication/ui/contacts/ContactsViewModel.kt
+++ app/src/main/java/com/example/contactapplication/ui/contacts/ContactsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.contactapplication.ui.dashboard
+package com.example.contactapplication.ui.contacts
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ContactsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
